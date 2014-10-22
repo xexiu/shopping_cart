394d77fdbe07c34e944042459a52d6d76e2e328d
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-10-22 18:40:50 +0200

    Added methods and some fixes

--- shopping_cart.rb
+++ shopping_cart.rb
@@ -1,2 +1,18 @@
-Class ShopCart
+class ShopCart
+  attr_accessor :list_item
+  def initialize
+    @list_item = {}
+  end
+  def addItem(name, quant)
+    return @list_item[name] = quant unless @list_item[name]
+    @list_item[name] += quant
+  end
 end
+end
+
+my_cart = ShopCart.new
+my_cart.addItem("banana", 1)
+# my_cart.addItem("banana", 1)
+puts my_cart.list_item
+my_cart.addItem("butter", 2)
+puts my_cart.list_item
