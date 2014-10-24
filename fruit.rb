e88678f00a042e5fd7dabaa9c9d0b36d235210f9
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-10-24 13:43:34 +0200

    Defined discount method

--- fruit.rb
+++ fruit.rb
@@ -4,4 +4,15 @@ class Fruit < Item
     super(name, price)
     @discount_percent = 10
   end
+
+  def apply_discount?
+    if Date.today.wday > 0 && Date.today.wday < 5
+      return false
+    else
+      return true
+    end
+    # Metodo de Ben: return false if (Date.today.wday.between?(1,5)) true
+    # monday = 1
+    # friday = 5
+  end
 end
